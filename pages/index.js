3682b29858adc78c891adafbed5919c7dd19f842
Leah Fernandez <109481045+leahmariefernandez@users.noreply.github.com>
2023-04-12 16:23:00 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <p>Check my <a className={utilStyles.click} href="https://github.com/leahmariefernandez">github</a>, for what I've been up to lately. </p>
         <p>Here is my <a className={utilStyles.click} href="./images/FernandezLeah.docx.pdf">resume</a>.</p>
         <p>More importantly, let’s play a <a className={utilStyles.click} href="./game">game.</a>(essay about life).</p>
-        <p><a className={utilStyles.click} href="https://drive.google.com/drive/folders/1cSDCaOrtDiNioVbrHWG5CvmTkurfR-0q?usp=share_link">Everyone</a> & <a className={utilStyles.click} href="https://parallel-telephone-ee8.notion.site/fa81dd23c210421cbb732a73c72b633d?v=7d7ba9731ccb4d7da71a681554cd237a">Everything</a></p>
+        <p><a className={utilStyles.click} href="https://curius.app/leah-fernandez">Everyone</a> & <a className={utilStyles.click} href="https://parallel-telephone-ee8.notion.site/fa81dd23c210421cbb732a73c72b633d?v=7d7ba9731ccb4d7da71a681554cd237a">Everything</a></p>
         <p>I hope you found everything you were looking for :)</p>
       </section>
     </Layout>
